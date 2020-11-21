8d9626b61931205ef5dde528225a243bcded4e98
teriyakisource <vassilisnin@gmail.com>
2020-11-21 11:25:10 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ console.log("running some stuff")
 const net = require('net');
 
 const server = net.createServer(conn => { console.log("New Client"); conn.on('data', data => {
-    conn.write(data + "\n");
-    console.log("Client: " + data);
-});
-conn.on('end', () => { console.log("client leave :(");
+        // conn.write();
+        console.log("Client: " + data + "\n");
+        switch (data)
+        {
+            case "bruh":
+                conn.write("lmao");
+
+            default:
+                conn.write("lmaaasdasddas")
+        };
+        
+
+    });
+    conn.on('end', () => {
+        console.log("client leave :(");
     });
 });
 server.listen(9090);
