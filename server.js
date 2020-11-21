31f7128adedb30f40cc7f3bb34fdd405c132bcf3
teriyakisource <vassilisnin@gmail.com>
2020-11-21 11:05:13 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const net = require('net');
 
-
-const server = net.createServer(conn => { console.log("New Client"); conn.on('data', data => { conn.write(data
-    + "\n");
-console.log("CLIENT: " + data);
-  });
-  conn.on('end', () => { console.log("client leave :(");
-  });
+const server = net.createServer(conn => { console.log("New Client"); conn.on('data', data => {
+    conn.write(data + "\n");
+    console.log("Client: " + data);
+});
+conn.on('end', () => { console.log("client leave :(");
+    });
 });
 server.listen(9090);
